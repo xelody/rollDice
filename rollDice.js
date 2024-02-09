580c25ade69068ca6b12077aa6fb2dd3585b7d20
xelody <melodypxin@gmail.com>
2024-02-09 09:51:47 -0800

    commit the rollDice function

---
+++ rollDice.js
@@ -0,0 +1,23 @@
+// Peiyao Xin
+
+function rollDice(input) {
+    const regex = /^(\d+)d(\d+)$/;
+    // invalid input
+    if (!regex.test(input)) {
+        throw new Error("Invalid input format. Please use 'NdM' format (eg. '3d6').");
+    }
+
+    const [numOfDices, numOfSides] = input.split('d').map(Number);
+    let total = 0;
+    for (let i = 0; i < numOfDices; i++) {
+        total += Math.floor(Math.random() * numOfSides) + 1;
+    }
+    return total;
+}
+
+const input = process.argv[2];
+try {
+    console.log(rollDice(input));
+} catch (error) {
+    console.error(error.message);
+}
